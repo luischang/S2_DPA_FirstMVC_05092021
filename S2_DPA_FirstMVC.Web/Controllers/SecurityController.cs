e21b187139515f9b1bc151cfdccf066543563b29
Luis Chang <luis.chang@outlook.com>
2021-09-07 10:06:21 -0500

    Agregar archivos de proyecto.

---
+++ S2_DPA_FirstMVC.Web/Controllers/SecurityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S2_DPA_FirstMVC.Web.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ S2_DPA_FirstMVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace S2_DPA_FirstMVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
