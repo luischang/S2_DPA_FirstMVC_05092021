dbcb37e09ee3a9502d77544688dfead5b39504ac
Luis Chang <luis.chang@outlook.com>
2021-09-12 13:04:23 -0500

    GetJSonProducts

--- S2_DPA_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
+++ S2_DPA_FirstMVC.Web/Areas/Marketing/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using S2_DPA_FirstMVC.Web.Areas.Marketing.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +11,23 @@ namespace S2_DPA_FirstMVC.Web.Areas.Marketing.Controllers
 {
     [Area("Marketing")]
     public class HomeController : Controller
-    {        
+    {
         public IActionResult Index()
         {
             return View();
         }
+
+
+
+
+        public IEnumerable<Product> GetJsonProducts()
+        {
+            var folder = Path.Combine(Directory.GetCurrentDirectory(), "Areas\\Marketing\\Data\\products.json");
+            var json = System.IO.File.ReadAllText(folder);
+            var products = JsonConvert.DeserializeObject<IEnumerable<Product>>(json);
+
+            return products;
+        }
+
     }
 }
